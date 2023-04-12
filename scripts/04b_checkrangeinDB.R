b340fbd9be90aa23daba473699b773b4d94fa67c
Benmm <ben.millardmartin@hakai.org>
2023-04-12 15:56:26 -0400

    push

--- scripts/04b_checkrangeinDB.R
+++ scripts/04b_checkrangeinDB.R
@@ -3,36 +3,47 @@ library(tidyverse)
 library(rfishbase)
 
 
-speclists <- read_csv("./processeddata/species_lists/region/20230410_obis_by_meow_ppow_NEP.csv")
+speclists <- read_csv("./processeddata/species_lists/region/20230412_obis-fish_NEP.csv")
 
-spec_fish <- speclists %>%
+#fish by region, add fishbase names
+fish <- speclists %>%
   filter(!is.na(species)) %>%
   filter(class %in% c('Actinopteri', 'Elasmobranchii', 'Holocephali',
                       'Petromyzonti', 'Myxini') |
-           order %in% c('Scorpaeniformes'))
+           order %in% c('Scorpaeniformes')) %>%
+  mutate(fishbase_name = validate_names(species_list = .$accepted_name_usage))
 
-#get distribution
-dist_fish <- distribution(species_list = spec_fish$species, server = 'fishbase')
-unique(dist_fish$FAO)
+#fishlist, add fishbase names
+spec_fish <- fish %>%
+  dplyr::select(-c('region')) %>%
+  distinct() %>%
+  mutate(fishbase_name = validate_names(species_list = .$accepted_name_usage))
+
+#get fishbase names
+spec_fish$fishbase_name <- validate_names(species_list = spec_fish$accepted_name_usage)
+
+#get distribution for fishbase names
+dist_fish <- distribution(species_list = spec_fish$fishbase_name, server = 'fishbase')
 
 #filter for species in region
-a1 <- dist_fish %>%
-  filter(FAO %in% 'Pacific, Northwest' |
-         FAO %in% 'Pacific, Eastern Central' |
-         FAO %in% 'America, North - Inland waters') %>%
+fishbase_in_FAO <- dist_fish %>%
+  filter(FAO %in% 'Pacific, Northeast' |
+           FAO %in% 'Pacific, Eastern Central' |
+           FAO %in% 'America, North - Inland waters') %>%
   select(c('Species')) %>%
   distinct()
 
-#
-a2 <- spec_fish$species[(!spec_fish$species %in% a1$Species)] %>%
-  as.data.frame()
+#filter dataset (fish by region) for fish confirmed by fishbase
+fish_by_region <- fish %>%
+  filter(fishbase_name %in% fishbase_in_FAO$Species)
+length(unique(fish_by_region$species)) #retained 2360 species
 
+#what fish are not in the region (according to fishbase) and where are they from?
+fishbase_not_in_FAO <- dist_fish %>%
+  filter(!Species %in% fishbase_in_FAO$Species)
+length(unique(fishbase_not_in_FAO$Species)) #removed 379 species
 
-
-
-
-
-
+write.csv(fish_by_region, "./processeddata/species_lists/region/20230412_obis-fish_fishbase_NEP.csv")
 
 spec_inv <- speclists %>%
   filter(!is.na(species)) %>%
