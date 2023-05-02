f585f6dfd2cdcb8350efce7b0065749eb952c622
Benmm <ben.millardmartin@hakai.org>
2023-05-01 20:13:32 -0400

    update blast

--- scripts/Python/blast_iterate.py
+++ scripts/Python/blast_iterate.py
@@ -13,23 +13,19 @@ from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio import SeqIO
 from Bio import SearchIO
 
-fasta = SeqIO.parse("top500_consensus.fasta", "fasta")
+fasta = open("top500_consensus.fasta").read()
+print(fasta)
+with open("ncbi_IDs_sub.txt", "r") as f2:
+        data = f2.read()
+print(data)
 
-
-record_iterator = SeqIO.parse("asv_table_sub.fasta", "fasta")
-print("1")
-#print(record_iterator)
-print("2")
-for record in record_iterator:
-    entry = str(">" + record.description + "\n" + record.seq)
-    print("entry")
+for i in data:
     blastn_cline = NCBIWWW.qblast("blastn", 
                                "nt",
-                               entry, 
-                               hitlist_size = 2, 
+                               fasta, 
+                               hitlist_size = 1, 
                                format_type = 'XML',
-                               entrez_query= "txid7898 [ORGN]")    
-#print(blastn_cline)
+                               entrez_query= i)
     with open('blast1.xml', 'w') as save_file: 
         blast_results = blastn_cline.read() #append
         save_file.write(blast_results)
@@ -42,5 +38,35 @@ for record in record_iterator:
     with open("blastn_all.tab", "a") as f:
         f.write(data)
     print("done")
+print("done")
+
+
+
+#record_iterator = SeqIO.parse("asv_table_sub.fasta", "fasta")
+#print("1")
+##print(record_iterator)
+#print("2")
+#for record in record_iterator:
+#    entry = str(">" + record.description + "\n" + record.seq)
+#    print("entry")
+#    blastn_cline = NCBIWWW.qblast("blastn", 
+#                               "nt",
+#                               entry, 
+#                               hitlist_size = 2, 
+#                               format_type = 'XML',
+#                               entrez_query= "txid7898 [ORGN]")    
+##print(blastn_cline)
+#    with open('blast1.xml', 'w') as save_file: 
+#        blast_results = blastn_cline.read() #append
+#        save_file.write(blast_results)
+##    print("3")
+#    qresults = SearchIO.parse("blast1.xml", "blast-xml")  # read XML file and parse
+#    SearchIO.write(qresults, "results4.tab", "blast-tab") # save as .tab
+#    with open("results4.tab", "r") as f2:
+#        data = f2.read()
+#    print(data)
+#    with open("blastn_all.tab", "a") as f:
+#        f.write(data)
+#    print("done")
 
 
