401435126ecda8b9fdba0c8468d8b2a9584b60d1
Lithobius <rage.of.anath@gmail.com>
2023-05-04 14:02:23 -0400

    added ncbi ids to obis search, ncbi fasta pull

--- scripts/Python/04_ncbi-query.py
+++ scripts/Python/04_ncbi-query.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-this is a basic template for a python script bc I'm lazy
-I also do the same thing a lot. Anyway, notes go here
+This script queries NCBI using Biopython.
+It requires a list of UIDS formatted: txid###[Orgn],txid###[Orgn]
+The list is queried then 3 sequences matching the query are written to a FASTA file.
+Refining the query process is still in progress.
 
 Any license info also goes here
 Author: "Kate Sheridan"
@@ -57,24 +59,28 @@ def fasta_by_acc(query_list, outfile_path):
         for q in query_list:
             logging.info(q + "is searching")
             handle = Entrez.esearch(
-                db='nucleotide', term=q + "AND 12S NOT UNVERIFIED [word] NOT PREDICTED [word]", usehistory="y", retmax="5", idtype="acc")
+                db='nucleotide', term=q + "AND 12S NOT UNVERIFIED [word] NOT PREDICTED [word]", usehistory="y", retmax="3", idtype="acc")
             search_results = Entrez.read(handle)
             handle.close()
             webenv = search_results["WebEnv"]
             querykey = search_results["QueryKey"]
             acc_list = search_results["IdList"]
             logging.info(acc_list)
-            fetch_handle = Entrez.efetch(
-                db="nucleotide",
-                rettype="fasta",
-                retmode="text",
-                webenv=webenv,
-                query_key=querykey,
-                idtype="acc",
-            )
-            data = fetch_handle.read()
-            fetch_handle.close()
-            out.write(data)
+            # if we didn't get any ids, don't write
+            if not acc_list:
+                pass
+            else:
+                fetch_handle = Entrez.efetch(
+                    db="nucleotide",
+                    rettype="fasta",
+                    retmode="text",
+                    webenv=webenv,
+                    query_key=querykey,
+                    idtype="acc",
+                    )
+                data = fetch_handle.read()
+                fetch_handle.close()
+                out.write(data)
 
 
 # basic-body
@@ -84,16 +90,16 @@ if __name__ == "__main__":
     # set emails
     Entrez.email = "kate.sheridan@mail.mcgill.ca"
     Entrez.api_key = "330c561fca772258d2c869e4caefe8c98b09"
-    # Path to fasta output
-    fasta_out = ("./processeddata/species_lists/ncbi/")
+    # Path to fasta output,
+    # name of fasta file currently defined in function (change)
+    fasta_out_path = ("./processeddata/species_lists/ncbi/")
     # path to txt for search terms
     search_terms = (
-        "processeddata/species_lists/ncbi/20230501_region-fish-ncbisearch2.txt")
+        "processeddata/species_lists/ncbi/20230501_region-fish-ncbisearch.txt")
     # search_terms = ("processeddata/species_lists/region/ncbi_IDs.txt")
     # get search terms
     query = get_search_terms(search_terms)
-    fasta_by_acc(query, fasta_out)
-
+    fasta_by_acc(query, fasta_out_path)
 
 
 # end
